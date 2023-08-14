d780d12856292a99e6c2c1d26753be7478e77118
DDMeaqua <lzlloveyou@vip.qq.com>
2023-08-14 17:43:44 +0800

    feat: update v2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,28 @@
-import React, {useEffect,useState} from 'react'
+import React , {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import {initI18n} from './i18n'
-import {bitable} from '@base-open/web-api'
+import './App.css';
+import 'normalize.css';
+import App from './App';
+import { initI18n } from './i18n'
+import { bitable } from '@base-open/web-api';
+import { useTranslation } from 'react-i18next';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <LoadApp/>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode><LoadApp/></React.StrictMode>)
+
+
 
 function LoadApp() {
+  
   const [load, setLoad] = useState(false);
   const [loadErr, setLoadErr] = useState<any>(null)
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoadErr(
-        <div>
-          运行项目后，请获取 webview 地址，粘贴至多维表格「扩展脚本」中使用。详见：
-          <a target='_blank' href='https://bytedance.feishu.cn/docx/HazFdSHH9ofRGKx8424cwzLlnZc'>开发指南</a>
-        </div>
-      )
-    }, 1000)
+      initI18n('en');
+      setTimeout(()=>{
+        setLoadErr(<LoadErr/>)
+        
+      },1000)
+    }, 5000)
     bitable.bridge.getLanguage().then((lang) => {
       clearTimeout(timer)
       initI18n(lang as any);
@@ -36,3 +37,13 @@ function LoadApp() {
 
   return loadErr
 }
+
+function LoadErr() {
+  const {t} = useTranslation();
+  return <div>
+            {t('load_error.1')}
+            <a target='_blank' href='https://bytedance.feishu.cn/docx/HazFdSHH9ofRGKx8424cwzLlnZc'>{t('load.guide')}</a>
+          </div>
+        
+}
+
